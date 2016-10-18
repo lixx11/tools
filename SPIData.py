8ede81943d21666d6957021d9d6bdd12e6b5d800
lixx11 <lxx2011011580@gmail.com>
2016-10-18 17:19:29 +0800

    add Nn, Ns, Nm and Np data

--- SPIData.py
+++ SPIData.py
@@ -4,6 +4,14 @@ Convert single particle patterns into hdf5 format file. The structure of the out
         3D data with shape (Np, Nx, Ny) where Np is the number of patters, and Nx, Ny are the size of first and second axis.
     'labels': 
         classification results of the data with shape (Np,). 0 for no particle, 1 for single particle, 2 for multiple particle
+    'Nn':
+        Number of paterns with no particle.
+    'Ns':
+        Number of patterns with single particle.
+    'Nm':
+        Number of patterns with multiple particles.
+    'Np':
+        Number of totoal patterns.
     'wavelength':
         Optional. The XFEL wavelength in angstrom(1E-10m).
     'detector distance':
@@ -30,6 +38,10 @@ class SPIData(object):
         self.h5File = self.initH5()
         self.patternShape = None
         self.labels = []
+        self.Nn = 0  # Num of no particle pattern
+        self.Ns = 0  # Num of single particle pattern
+        self.Nm = 0  # Num of multiple particle pattern
+        self.Np = 0  # Num of total patterns
 
     def initH5(self):
         h5File = h5py.File(self.output, 'w')
@@ -58,34 +70,42 @@ class SPIData(object):
         pattern = np.asarray(pattern)
         assert len(pattern.shape) == 2  # must be 2D pattern
         label = int(label)
-        if label == 0:
-            print('adding no particle pattern')
-        elif label == 1:
-            print('adding single particle pattern')
-        elif label == 2:
-            print('adding multiple particle pattern')
-        else:
-            raise ValueError('label must be 0, 1 or 2 for no, single and multiple particle pattern')
+        if self.patternShape is not None and pattern.shape != self.patternShape:
+            print('Warning!!! Pattern has different shape. Discard this pattern')  # the pattern shape must be same in one dataset
+            return None
+        if label not in [0,1,2]:
+            raise ValueError('Label must be 0, 1 or 2 for no, single and multiple particle pattern')
 
-        if self.patternShape is None:
-            self.patternShape = pattern.shape
-            print('pattern shape is set to %s' %str(pattern.shape))
-            newShape = (1, pattern.shape[0], pattern.shape[1])
-            pattern = pattern.reshape(newShape)
+        if label == 0:
+            print('Adding no particle pattern.')
+            self.Nn += 1
+        elif label == 1:
+            print('Adding single particle pattern.')
+            self.Ns += 1
+        elif label == 2:
+            print('Adding multiple particle pattern.')
+            self.Nm += 1
+        self.labels.append(label)
+
+        newShape = (1, pattern.shape[0], pattern.shape[1])
+        pattern = pattern.reshape(newShape)
+        if self.Np == 0:
+            self.patternShape = pattern.shape[1:]
+            print('First adding. Pattern shape is set to %s' %str(pattern.shape))
             self.h5File.create_dataset('data', data=pattern, maxshape=(None, self.patternShape[0], self.patternShape[1]), chunks=newShape)
-            self.labels.append(label)
         else:
-            if pattern.shape != self.patternShape:
-                print('Warning!!! Pattern has different shape. Discard this pattern')  # the pattern shape must be same in one dataset
-                return None
-            Np = self.h5File['data'].shape[0]
-            self.h5File['data'].resize(Np+1, axis=0)
-            self.h5File['data'][Np] = pattern
+            self.h5File['data'].resize(self.Np+1, axis=0)
+            self.h5File['data'][self.Np] = pattern
             self.labels.append(label)
+        self.Np += 1
 
     def close(self):
         labels = np.asarray(self.labels, dtype=np.int8)
         self.h5File.create_dataset('labels', data=labels)
+        self.h5File.create_dataset('Nn', data=self.Nn)
+        self.h5File.create_dataset('Ns', data=self.Ns)
+        self.h5File.create_dataset('Nm', data=self.Nm)
+        self.h5File.create_dataset('Np', data=self.Np)
         self.h5File.close()
 
 
