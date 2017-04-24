06b1f57d7d5cb9fd3d3fba198449f68129ea123b
lixx11 <lxx2011011580@gmail.com>
2017-04-24 19:26:52 +0800

    [update] test stream visualisation; state save/load support;

--- cxi_viewer/cxi_viewer.py
+++ cxi_viewer/cxi_viewer.py
@@ -13,6 +13,22 @@ import numpy as np
 from stream_parser import parse_stream
 
 
+# pens for plot items, visit 
+# http://www.pyqtgraph.org/documentation/functions.html
+# for more details
+pens = {'peak': '#ff0000',
+        'ref': '#5dff35',
+        'test1': '#35fffb',
+        'test2': '#3496ff',
+        'test3': '#ddff00'}
+
+# sizes for reflection plot
+sizes = {'peak': 3,
+        'ref': 5,
+        'test1': 7,
+        'test2': 9,
+        'test3': 11}
+
 def load_data_from_stream(filename):
     """load reflection information from stream file"""
     chunks = []
@@ -85,13 +101,14 @@ class Geometry(object):
             f = h5py.File('.geom.h5', 'r')
             return f['x'].value, f['y'].value, f['z'].value
         else:
-            print('Wrong geometry: %s. You must provide Cheetah, CrystFEL or psana geometry file.')
+            print('Wrong geometry: %s. You must provide Cheetah, \
+                CrystFEL or psana geometry file.')
             return None
         
 
 class CXIWindow(QtGui.QMainWindow):
     """docstring for CXIWindow"""
-    def __init__(self):
+    def __init__(self, state_file):
         super(CXIWindow, self).__init__()
         # load and adjust layout
         dir_ = os.path.dirname(os.path.abspath(__file__))
@@ -103,6 +120,8 @@ class CXIWindow(QtGui.QMainWindow):
         self.actionLoadGeom.triggered.connect(self.loadGeomSlot)
         self.actionRefStream.triggered.connect(self.loadRefStreamSlot)
         self.actionTestStream.triggered.connect(self.loadTestStreamSlot)
+        self.actionLoadState.triggered.connect(self.loadStateSlot)
+        self.actionSaveState.triggered.connect(self.saveStateSlot)
         # initialize parameters
         self.frame = 0
         self.pixel_size = 110E-6  # 110um pixel of CSPAD
@@ -114,6 +133,7 @@ class CXIWindow(QtGui.QMainWindow):
         self.reflection_items = []
 
         # stream plot
+        self.streamPlot.getPlotItem().setTitle('Stream Plot')
         self.scatterItem = pg.ScatterPlotItem()
         self.streamPlot.addItem(self.scatterItem)
         self.scatterItem.sigClicked.connect(self.scatterItemClicked)
@@ -141,14 +161,14 @@ class CXIWindow(QtGui.QMainWindow):
                         ]},
                         {'name': 'Basic Operation', 'type': 'group', 'children': [
                             {'name': 'Frame', 'type': 'int', 'value': self.frame},
+                        ]},
+                        {'name': 'Display Option', 'type': 'group', 'children': [
                             {'name': 'Show Reflections from', 'type': 'group', 'children': [
                                 {'name': 'Reference Stream', 'type': 'bool', 'value': self.showReflection['ref_stream']},
                                 {'name': 'Test Stream 1', 'type': 'bool', 'value': self.showReflection['test_stream1']},
                                 {'name': 'Test Stream 2', 'type': 'bool', 'value': self.showReflection['test_stream2']},
                                 {'name': 'Test Stream 3', 'type': 'bool', 'value': self.showReflection['test_stream3']},
-                            ]}
-                        ]},
-                        {'name': 'Display Option', 'type': 'group', 'children': [
+                            ]},
                             {'name': 'AutoRange', 'type': 'bool', 'value': self.dispOption['autoRange']},
                             {'name': 'AutoLevel', 'type': 'bool', 'value': self.dispOption['autoLevels']},
                             {'name': 'AutoHistogram', 'type': 'bool', 'value': self.dispOption['autoHistogramRange']},
@@ -157,39 +177,134 @@ class CXIWindow(QtGui.QMainWindow):
         self.params = Parameter.create(name='params', type='group', children=params_list)
         self.parameterTree.setParameters(self.params, showTop=False)
         # parameter connection
-        self.params.param('Basic Operation', 'Frame').sigValueChanged.connect(self.frameChangedSlot)
+        self.params.param('Basic Operation', 
+            'Frame').sigValueChanged.connect(self.frameChangedSlot)
+        self.params.param('Display Option', 
+            'Show Reflections from', 
+            'Reference Stream').sigValueChanged.connect(self.showRefSlot)
+        self.params.param('Display Option', 
+            'Show Reflections from', 
+            'Test Stream 1').sigValueChanged.connect(self.showTest1Slot)
+        self.params.param('Display Option', 
+            'Show Reflections from', 
+            'Test Stream 2').sigValueChanged.connect(self.showTest2Slot)
+        self.params.param('Display Option', 
+            'Show Reflections from', 
+            'Test Stream 3').sigValueChanged.connect(self.showTest3Slot)
+        # mouse move connection
+        self.imageView.scene.sigMouseMoved.connect(self.mouseMoved)
+        # load state if given
+        self.maybeLoadState(state_file)
+
+    def mouseMoved(self, pos):
+        if self.cxi_file is None:
+            return
+        mouse_pos = self.imageView.view.mapToView(pos)
+        image = self.imageView.image
+        x, y = int(mouse_pos.x()), int(mouse_pos.y())
+        if 0 <= x < image.shape[0] and 0 <= y < image.shape[1]:
+            self.statusbar.showMessage("x:%d y:%d I:%.2E" % 
+                (x, y, image[x, y]), 5000)
+
+    def showRefSlot(self, _, show):
+        self.showReflection['ref_stream'] = show
+        self.updateDisp()
+
+    def showTest1Slot(self, _, show):
+        self.showReflection['test_stream1'] = show
+        self.updateDisp()
+
+    def showTest2Slot(self, _, show):
+        self.showReflection['test_stream2'] = show
+        self.updateDisp()
+
+    def showTest3Slot(self, _, show):
+        self.showReflection['test_stream3'] = show
+        self.updateDisp()
 
     def scatterItemClicked(self, _, points):
         pos = points[0].pos()
         index = int(points[0].pos()[0])
         print('clicked %d' % index)
-        self.frame = index
-        self.updateDisp()
+        self.params.param('Basic Operation', 
+            'Frame').setValue(index)
 
     def frameChangedSlot(self, _, frame):
         self.frame = frame
         self.updateDisp()
 
+    def loadStateSlot(self):
+        """Load configuration from state file"""
+        fpath = str(QtGui.QFileDialog.getOpenFileName(self, 
+            'Open File', '', 'State File (*.yml)'))
+        self.maybeLoadState(fpath)
+
+    def maybeLoadState(self, fpath):
+        import yaml
+        yml_file = file(fpath, 'r')
+        state = yaml.safe_load(yml_file)
+        yml_file.close()
+        self.maybeLoadCXI(state['cxi_file'])
+        self.maybeLoadGeom(state['geom_file'])
+        self.maybeLoadRefStream(state['ref_stream_file']) 
+        self.maybeLoadTestStream(state['test_stream_files'])
+
+    def saveStateSlot(self):
+        """Save configuration to state file"""
+        import yaml
+        state = dict()
+        state['cxi_file'] = self.cxi_file
+        state['geom_file'] = self.geom_file
+        state['ref_stream_file'] = self.ref_stream_file
+        state['test_stream_files'] = self.test_stream_files
+        fpath = QtGui.QFileDialog.getSaveFileName(self, 
+            'Save File', '', 'State File (*.yml)')
+        yml_file = file(fpath, 'w')
+        yaml.dump(state, yml_file)
+        yml_file.close()
+
     def loadTestStreamSlot(self):
         """Load test stream files (<=3)"""
-        fpaths = QtGui.QFileDialog.getOpenFileNames(self, 'Open file', '', 'Stream File (*.stream)')
+        fpaths = QtGui.QFileDialog.getOpenFileNames(self, 
+            'Open File', '', 'Stream File (*.stream)')
+        self.maybeLoadTestStream(fpaths)
+
+    def maybeLoadTestStream(self, fpaths):
+        if len(fpaths) == 0:
+            return
         self.test_stream_files = []
         self.test_stream_data = []
-        for fpath in fpaths:
+        for i in range(len(fpaths)):
+            fpath = str(fpaths[i])
             self.test_stream_files.append(str(fpath))
             self.test_stream_data.append(load_data_from_stream(str(fpath)))
+            self.params.param('File Info', 
+                'Test Stream %d' % (i+1)).setValue(basename(str(fpath)))
         self.updateStreamPlot()
 
     def loadRefStreamSlot(self):
-        fpath = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '', 'Stream File (*.stream)')
+        fpath = str(QtGui.QFileDialog.getOpenFileName(self, 
+            'Open File', '', 'Stream File (*.stream)'))
+        self.maybeLoadRefStream(fpath)
+
+    def maybeLoadRefStream(self, fpath):
+        if fpath is '':
+            return
         self.ref_stream_file = str(fpath)
         self.ref_stream_data = load_data_from_stream(self.ref_stream_file)
-        self.params.param('File Info', 'Reference Stream').setValue(basename(self.ref_stream_file))
+        self.params.param('File Info', 
+            'Reference Stream').setValue(basename(self.ref_stream_file))
         self.updateStreamPlot()
 
     def loadCXISlot(self):
-        fpath = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '', 'CXI File (*.cxi)')
-        self.cxi_file = str(fpath)
+        fpath = str(QtGui.QFileDialog.getOpenFileName(self, 
+            'Open File', '', 'CXI File (*.cxi)'))
+        self.maybeLoadCXI(fpath)
+
+    def maybeLoadCXI(self, fpath):
+        if fpath is '':
+            return
+        self.cxi_file = fpath
         cxi = h5py.File(self.cxi_file, 'r')
         self.data = cxi['/entry_1/instrument_1/detector_1/detector_corrected/data']
         self.nb_pattern = self.data.shape[0]
@@ -201,18 +316,22 @@ class CXIWindow(QtGui.QMainWindow):
         self.updateStreamPlot()
 
     def loadGeomSlot(self):
-        fpath = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file', '', 'Geom File (*.geom *.h5 *.data)'))
+        fpath = str(QtGui.QFileDialog.getOpenFileName(self, 
+            'Open file', '', 'Geom File (*.geom *.h5 *.data)'))
+        self.maybeLoadGeom(fpath)
+
+    def maybeLoadGeom(self, fpath):
         if fpath is '':
             return
         self.geom_file = fpath
         self.geom = Geometry(self.geom_file, self.pixel_size)
-        self.params.param('File Info', 'Geometry File').setValue(basename(self.geom_file))
+        self.params.param('File Info', 
+            'Geometry File').setValue(basename(self.geom_file))
         self.updateDisp()
 
-    def testSlot(self):
-        print('hello world')
-
-    def render_reflections(self, stream_data, frame, anchor=(0, 0), pen='g', tag=''):
+    def render_reflections(self, stream_data, frame, 
+        anchor=(0, 0), pen='r', size=5, tag=''):
+        """Render reflections from stream file"""
         if not stream_data.has_key(frame):
             return None  # this event not exist
         event = stream_data[frame]
@@ -220,7 +339,7 @@ class CXIWindow(QtGui.QMainWindow):
         Is = event['Is']
         print('%d reflections including %d negative tagged by %s' 
                % (Is.size, (Is < 0.).sum(), tag))
-        spots_x, spots_y = list(), list()
+        spots_x, spots_y = [], []
         for j in range(Is.size):
             spot_x, spot_y = self.geom.map((rawXYs[j,:]))
             spots_x.append(spot_x)
@@ -228,21 +347,44 @@ class CXIWindow(QtGui.QMainWindow):
         p = pg.ScatterPlotItem()
         self.imageView.getView().addItem(p)
         self.reflection_items.append(p)
-        p.setData(spots_x, spots_y, symbol='o', size=10, brush=(255,255,255,0), pen=pen)
+        p.setData(spots_x, spots_y, symbol='o', 
+            size=size, brush=(255,255,255,0), pen=pen)
 
     def updateStreamPlot(self):
         y = 0
         self.scatterItem.clear()
+        ticks = []
+        ax = self.streamPlot.getAxis('left')
+        # scatter for all patterns
         if self.cxi_file is not None:
-            self.scatterItem.addPoints(np.arange(self.nb_pattern), np.ones(self.nb_pattern)*y)
+            self.scatterItem.addPoints(
+                np.arange(self.nb_pattern), 
+                np.ones(self.nb_pattern)*y,
+                pen=pens['peak'])
+            ticks.append((0, 'all'))
             y += 1
+        # scatter for reference stream
         if self.ref_stream_file is not None:
             event_ids = self.ref_stream_data.keys()
             p = pg.ScatterPlotItem()
             self.streamPlot.addItem(p)
-            self.scatterItem.addPoints(event_ids, np.ones_like(event_ids)*y)
+            self.scatterItem.addPoints(
+                event_ids, np.ones_like(event_ids)*y,
+                pen=pens['ref'])
+            ticks.append((1, 'ref'))
             y += 1
-        # todo: render test streams
+        # scatter for test streams
+        if len(self.test_stream_files) > 0:
+            for i in range(len(self.test_stream_data)):
+                event_ids = self.test_stream_data[i].keys()
+                p = pg.ScatterPlotItem()
+                self.streamPlot.addItem(p)
+                self.scatterItem.addPoints(
+                    event_ids, np.ones_like(event_ids)*y,
+                    pen=pens['test%d' % (i+1)])
+                ticks.append((i+2, 'test%d' % (i+1)))
+                y += 1
+        ax.setTicks([ticks])
 
     def updateDisp(self):
         # clean peaks and reflections in last frame
@@ -255,13 +397,14 @@ class CXIWindow(QtGui.QMainWindow):
         image = self.data[self.frame]
         if self.geom_file is not None:
             image = self.geom.rearrange(image)
-            image[0,0] = 500.
-        self.imageView.setImage(image, autoRange=self.dispOption['autoRange'],
-                             autoLevels=self.dispOption['autoLevels'],
-                             autoHistogramRange=self.dispOption['autoHistogramRange'])
+            image[0,0] = 500.  # set a reference point
+        self.imageView.setImage(image, 
+            autoRange=self.dispOption['autoRange'],
+            autoLevels=self.dispOption['autoLevels'],
+            autoHistogramRange=self.dispOption['autoHistogramRange'])
         # render peaks extracted from cxi file
         n_peaks = 0
-        peaks_x, peaks_y = list(), list()
+        peaks_x, peaks_y = [], []
         for i in range(len(self.peaks_x[self.frame])):
             peak_x = int(round(self.peaks_x[self.frame][i]))
             peak_y = int(round(self.peaks_y[self.frame][i]))
@@ -277,21 +420,42 @@ class CXIWindow(QtGui.QMainWindow):
                 n_peaks += 1
         p = pg.ScatterPlotItem()
         self.imageView.getView().addItem(p)
-        p.setData(peaks_x, peaks_y, symbol='x', size=6, brush=(255,255,255,0), pen='r')
+        p.setData(peaks_x, peaks_y, symbol='x', 
+            size=6, brush=(255,255,255,0), pen=pens['peak'])
         self.peak_items.append(p)
-        print('%d peaks found in %d/%d frame' % (n_peaks, self.frame, self.data.shape[0]))
+        print('%d peaks found in %d/%d frame' % 
+            (n_peaks, self.frame, self.data.shape[0]))
         # render reference stream reflections
         if self.showReflection['ref_stream']:
             if self.ref_stream_file is not None:
-                self.render_reflections(self.ref_stream_data, self.frame, tag='ref', anchor=(1.1,1.5), pen='g')
-            else:
-                pass
-        # todo: render test stream reflections
+                self.render_reflections(self.ref_stream_data, 
+                    self.frame, tag='ref', size=sizes['ref'], 
+                    anchor=(1.1,1.5), pen=pens['ref'])
+        # render test stream reflectins
+        if self.showReflection['test_stream1']:
+            if len(self.test_stream_files) >= 1:
+                self.render_reflections(self.test_stream_data[0], 
+                    self.frame, tag='test1', size=sizes['test1'],
+                    anchor=(1.1,1.5), pen=pens['test1'])
+        if self.showReflection['test_stream2']:
+            if len(self.test_stream_files) >= 2:
+                self.render_reflections(self.test_stream_data[1], 
+                    self.frame, tag='test1', size=sizes['test2'],
+                    anchor=(1.1,1.5), pen=pens['test2'])
+        if self.showReflection['test_stream3']:
+            if len(self.test_stream_files) >= 3:
+                self.render_reflections(self.test_stream_data[2], 
+                    self.frame, tag='test1', size=sizes['test3'],
+                    anchor=(1.1,1.5), pen=pens['test3'])
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    win = CXIWindow()
+    if len(sys.argv) == 2:
+        state_file = sys.argv[1]
+    else:
+        state_file = ''
+    win = CXIWindow(state_file)
     win.setWindowTitle("CXI Viewer")
     win.show()
     app.exec_()
