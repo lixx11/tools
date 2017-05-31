3c99640692c79dc46feb11e0836edbd88c183726
lixx11 <lxx2011011580@gmail.com>
2017-05-31 10:15:35 +0800

    remove hard code

--- extract_peaks.py
+++ extract_peaks.py
@@ -2,16 +2,17 @@
 
 """
 Usage:
-    extract_peaks.py -f data.cxi -g geom.h5 [options]
+    extract_peaks.py -i cxi_file_or_dir -g geometry_file -d det_dist [options]
 
 Options:
-    -h --help                                        Show this screen.
-    -f data.cxi                                      Image file in cxi format.
-    -g geom.h5                                       Geometry file in hdf5 format.
-    -o output_dir                                    Output directory [default: output].
-    --start=<start_event>                            Start event id [default: 0].
-    --sort-by=<metric>                               Sort peaks by certain metric [default: SNR].
-    --res=<resolution>                               Resolution threshold for high priority peaks in angstrom [default: 4.5].
+    -h --help                                       Show this screen.
+    -i cxi_file_or_dir                              CXI file or directory.
+    -g geometry_file                                Geometry file in cheetah, crystfel or psana format.
+    -d det_dist                                     Detector distance in meters.
+    -p pixel_size                                   Pixel size in meters [default: 110E-6].
+    -o output_dir                                   Output directory [default: output].
+    --sort-by=<metric>                              Sort peaks by certain metric [default: SNR].
+    --res=<resolution>                              Resolution threshold for high priority peaks in angstrom [default: 4.5].
 """
 
 from docopt import docopt
@@ -20,11 +21,7 @@ import h5py
 from tqdm import tqdm  # show a progress bar for loops
 import numpy as np
 from math import sin, atan, sqrt
-
-
-S_p = 110E-6  # pixel size in meters of CSPAD
-d = 136.4028E-3  # detector distance in meters
-lam = 1.306098E-10  # XFEL wavelength in meters
+import glob
 
 
 class Peak(object):
@@ -51,38 +48,55 @@ class Peak(object):
         self.photon_energy = photon_energy
 
 
-if __name__ == '__main__':
-    argv = docopt(__doc__)
-    cxi_file = argv['-f']
-    geom_file = argv['-g']
-    sort_by = argv['--sort-by']
-    res_threshold = float(argv['--res']) * 1.E-10  # convert resolution in angstroms to meters.
-    start_event = int(argv['--start'])
+def parse_peak_list_filename(filename):
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    experiment, run_id, class_id = basename.split('-')
+    class_id = class_id.split('e')[0]  # remove event id
+    return experiment, run_id, class_id
 
-    # load cxi file
+
+def load_geom(filename):
+    print "load %s" % filename
+    ext = os.path.splitext(filename)[1]
+    if ext == '.h5':
+        f = h5py.File(filename, 'r')
+        return f['x'].value, f['y'].value, f['z'].value
+    elif ext == '.geom':
+        from psgeom import camera
+        cspad = camera.Cspad.from_crystfel_file(filename)
+        cspad.to_cheetah_file('.geom.h5')
+        f = h5py.File('.geom.h5', 'r')
+        return f['x'].value, f['y'].value, f['z'].value
+    elif ext == '.psana':
+        from psgeom import camera
+        cspad = camera.Cspad.from_psana_file(filename)
+        cspad.to_cheetah_file('.geom.h5')
+        f = h5py.File('.geom.h5', 'r')
+        return f['x'].value, f['y'].value, f['z'].value
+    else:
+        print('Wrong geometry: %s. You must provide Cheetah, CrystFEL or psana geometry file.')
+        return None
+
+
+def extract_peaks(cxi_file, output_dir, sort_by='SNR', res_threshold=4.5E-10):
     data = h5py.File(cxi_file,'r')
     basename, ext = os.path.splitext(os.path.basename(cxi_file))
 
-    # load geometry
-    geom = h5py.File(geom_file, 'r')
-    geom_x, geom_y = geom['x'].value, geom['y'].value
-
     # mkdir for output
-    output_dir = argv['-o']
     if not os.path.isdir(output_dir):
         os.makedirs('%s' % output_dir)
 
-    nb_events = data['/cheetah/event_data/hit'][:].size
+    nb_events = data['entry_1/data_1/data'].shape[0]
     print('Processing %s with geometry %s' % (cxi_file, geom_file))
     for event_id in tqdm(range(nb_events)):
-        output = output_dir + '/' + basename + 'e' + str(event_id + start_event) + '.txt'
+        output = output_dir + '/' + basename + '-e%04d.txt' % event_id
         nb_peaks = np.nonzero(data['entry_1/result_1/peakTotalIntensity'][event_id][:])[0].size
         peak_list = []
         for peak_id in range(nb_peaks):
             rawX = int(data['/entry_1/result_1/peakXPosRaw'][event_id][peak_id])
             rawY = int(data['/entry_1/result_1/peakYPosRaw'][event_id][peak_id])
-            assX = geom_x[rawY][rawX] / S_p
-            assY = geom_y[rawY][rawX] / S_p
+            assX = geom_x[rawY][rawX] / pixel_size
+            assY = geom_y[rawY][rawX] / pixel_size
         
             total_intensity = data['/entry_1/result_1/peakTotalIntensity'][event_id][peak_id]
             SNR = data['/entry_1/result_1/peakSNR'][event_id][peak_id]
@@ -90,11 +104,23 @@ if __name__ == '__main__':
             photon_energy = data['LCLS/photon_energy_eV'][event_id]
             # calculate resolution
             r = sqrt(assX ** 2. + assY ** 2.)
-            res = lam / (sin(0.5 * atan(r * S_p / d)) * 2)
+            h = 4.135667662E-15  # Planck constant in eV*s
+            c = 2.99792458E8  # light speed in meters per sec
+            lam = h * c / photon_energy
+            res = lam / (sin(0.5 * atan(r * pixel_size / det_dist)) * 2)
             peak = Peak(assX, assY, res, SNR, total_intensity, encoder_value, photon_energy)
             peak_list.append(peak)
 
-        peak_list.sort(key=lambda peak: peak.SNR, reverse=True)
+        if sort_by == 'SNR':
+            peak_list.sort(key=lambda peak: peak.SNR, reverse=True)
+        elif sort_by == 'total_intensity':
+            peak_list.sort(key=lambda peak: peak.total_intensity, reverse=True)
+        elif sort_by == 'res':
+            peak_list.sort(key=lambda peak: peak.total_intensity, reverse=False)
+        else:
+            print('Unimplemented sort strategy: %s' % sort_by)
+            return None
+
         HP_ids = []  # high priority peak indices 
         LP_ids = []  # low priority peak indices
         for peak_id in range(len(peak_list)):
@@ -116,3 +142,24 @@ if __name__ == '__main__':
                     (peak.x, peak.y, peak.total_intensity, peak.SNR, peak.photon_energy, peak.encoder_value))
 
 
+if __name__ == '__main__':
+    argv = docopt(__doc__)
+    cxi_file_or_dir = argv['-i']
+    geom_file = argv['-g']
+    sort_by = argv['--sort-by']
+    res_threshold = float(argv['--res']) * 1.E-10  # convert resolution in angstroms to meters.
+    output_dir = argv['-o']
+    det_dist = float(argv['-d'])
+    pixel_size = float(argv['-p'])
+    # load geometry
+    geom_x, geom_y, geom_z = load_geom(geom_file)
+
+    # load cxi file
+    if os.path.isdir(cxi_file_or_dir):  # extract peaks from multiple cxi files
+        cxi_files = glob.glob(cxi_file_or_dir + '/' + '*.cxi')
+        for cxi_file in cxi_files:
+            experiment, run_id, class_id = parse_peak_list_filename(cxi_file)
+            extract_peaks(cxi_file, output_dir+'/'+class_id, sort_by=sort_by, res_threshold=res_threshold)
+    else:  # extract peaks from single cxi file
+        cxi_file = cxi_file_or_dir
+        extract_peaks(cxi_file_or_dir, output_dir+'/'+'c00', sort_by=sort_by, res_threshold=res_threshold)
